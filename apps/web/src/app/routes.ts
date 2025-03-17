2d31346b34fa2ef41297925e443c87247a30a2a2
ddi4z <d.diazm@uniandes.edu.co>
2025-03-17 16:46:50 -0500

    feature/master-thesis-form-#33

--- apps/web/src/app/routes.ts
+++ apps/web/src/app/routes.ts
@@ -35,9 +35,9 @@ export const ROUTES: Record<string, string> = {
   PROFESSOR_UNDERGRADUATE_THESIS_DATES: "/profesor/pregrado/fechas",
 
   PROFESSOR_POSTGRADUATE_THESIS_LIST: "/profesor/maestria/lista",
+  PROFESSOR_POSTGRADUATE_THESIS_NEW: "/profesor/maestria/nuevo",
   PROFESSOR_POSTGRADUATE_THESIS_HISTORY: "/profesor/maestria/historico",
   PROFESSOR_POSTGRADUATE_THESIS_DATES: "/profesor/maestria/fechas",
-  PROFESSOR_POSTGRADUATE_THESIS_PUBLISH_CONSULT_STUDENT: "/profesor/maestria/publicar/estudiante",
 
   PROFESSOR_ASSISTANCE_LIST: "/profesor/asistencias/lista",
   PROFESSOR_NEW_ASSISTANCE: "/profesor/asistencias/nueva",
