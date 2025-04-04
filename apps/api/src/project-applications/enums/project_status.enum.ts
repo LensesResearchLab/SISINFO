9d38a176c2eb396d18c9e94462df9e08fcd39967
ddi4z <d.diazm@uniandes.edu.co>
2025-04-04 11:05:35 -0500

    add/project-status-enumeration

--- apps/api/src/project-applications/enums/project_status.enum.ts
+++ apps/api/src/project-applications/enums/project_status.enum.ts
@@ -1,3 +1,7 @@
 export enum ProjecStatusEnum {
-  APPLICANT = 'Applicant',
+  APPLICANT = 'Estudiante aplico a proyecto',
+  WAITING = 'Esperando aprobación del profesor',
+  APPROVED = 'Proyecto aprobado por profesor',
+  REJECTED = 'Proyecto rechazado por profesor',
+  ENROLLED = 'Estudiante inscrito',
 }
