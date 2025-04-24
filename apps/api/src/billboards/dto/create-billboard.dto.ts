485fcc826f056f2416f8cfd1818678adb74377ce
Jorge David Bustamante Pino <111013638+JorgeB-py@users.noreply.github.com>
2025-04-23 22:31:51 -0500

    Fixed: billboard update and sections (#71)

--- apps/api/src/billboards/dto/create-billboard.dto.ts
+++ apps/api/src/billboards/dto/create-billboard.dto.ts
@@ -5,37 +5,6 @@ import {
 } from 'class-validator';
 
 export class CreateBillboardDto {
-  @IsString()
-  @IsNotEmpty()
-  section: string;
-
-  @IsString()
-  @IsNotEmpty()
-  NRC: string;
-
-  @IsString()
-  @IsNotEmpty()
-  code: string;
-
-  @IsString()
-  @IsNotEmpty()
-  name: string;
-
-  @IsString()
-  @IsNotEmpty()
-  departament: string;
-
-  @IsString()
-  @IsNotEmpty()
-  credits: number;
-
-  @IsString()
-  @IsNotEmpty()
-  period: string;
-
-  @IsString()
-  professors: string;
-
   @IsBoolean()
   publicated: boolean;
 }
